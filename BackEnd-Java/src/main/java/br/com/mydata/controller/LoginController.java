f701820950ebe20365d4ed1f3d9593f8dc924c1a
lucas.antunes <lucas.antunes@primeup.com.br>
2019-10-03 20:48:51 -0300

    Metodo de autenticação de login

--- BackEnd-Java/src/main/java/br/com/mydata/controller/LoginController.java
+++ BackEnd-Java/src/main/java/br/com/mydata/controller/LoginController.java
@@ -1,5 +1,29 @@
 package br.com.mydata.controller;
 
-public class LoginController {
+import java.util.UUID;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mydata.model.Usuario;
+
+@RestController
+@RequestMapping("login")
+public class LoginController {
+	
+	@PostMapping("autenticacao")
+	public Boolean login(@RequestBody Usuario usuario) {
+		Usuario mock = new Usuario();
+		mock.setId(UUID.randomUUID().toString());
+		mock.setNome("Joao");
+		mock.setSenha("1234");
+		
+		if(usuario.getNome().equals(mock.getNome()) && usuario.getSenha().equals(mock.getSenha())) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
---
+++ BackEnd-Java/src/main/java/br/com/mydata/model/Usuario.java
@@ -0,0 +1,27 @@
+package br.com.mydata.model;
+
+public class Usuario {
+
+	private String id;
+	private String nome;
+	private String senha;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String name) {
+		this.nome = name;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
