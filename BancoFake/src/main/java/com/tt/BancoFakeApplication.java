a84329f192535fcd6ea5d23b7856ffebfb38d9cb
Lucas Antunes <lucas.antunes@primeup.com.br>
2019-12-09 19:53:27 -0300

    Cria 2 empresa

---
+++ BancoFake/src/main/java/com/tt/BancoFakeApplication.java
@@ -0,0 +1,13 @@
+package com.tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFakeApplication.class, args);
+	}
+
+}
